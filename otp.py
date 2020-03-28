11fff5edea02c3a42a58286de60fb3c6b76ee888
Paper <paper@tilde.institute>
2020-03-28 21:59:09 +0100

    Get rid of pyotp dependency

--- otp.py
+++ otp.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import os
-import pyotp
 import subprocess
 import sys
+import time
+import base64
+import struct
+import hmac
 # idea: ~/.otp/github.gpg stores a OTP secret key for github
 # the file is GPG encypted
 # for example command "otp github" will give you a otp code
+
 if len(sys.argv) != 2:
     print("usage: otp [name]")
     sys.exit(1)
@@ -14,5 +18,17 @@ account=sys.argv[1]
 result = subprocess.Popen(["gpg2", "-d", f'{os.getenv("HOME")}/.otp/{account}.gpg'], stdout=subprocess.PIPE).stdout.read()
 secret = result.decode("utf-8").replace("\n","")
 
-totp = pyotp.TOTP(secret)
-print(totp.now())
+# hotp and totp functions are taken from https://github.com/susam/mintotp
+def hotp(key, counter, digits=6, digest='sha1'):
+    key = base64.b32decode(key.upper() + '=' * ((8 - len(key)) % 8))
+    counter = struct.pack('>Q', counter)
+    mac = hmac.new(key, counter, digest).digest()
+    offset = mac[-1] & 0x0f
+    binary = struct.unpack('>L', mac[offset:offset+4])[0] & 0x7fffffff
+    return str(binary)[-digits:].rjust(digits, '0')
+
+
+def totp(key, time_step=30, digits=6, digest='sha1'):
+    return hotp(key, int(time.time() / time_step), digits, digest)
+
+print(totp(secret))
